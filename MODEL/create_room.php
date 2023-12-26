69c597261c5105812f265f8a9fd3122c05a39c11
Mohammed Elghanam <144786058+MohammedElghanam@users.noreply.github.com>
2023-12-26 12:23:38 +0100

    79

--- MODEL/create_room.php
+++ MODEL/create_room.php
@@ -2,6 +2,18 @@
 include 'config.php';
 class create_room extends config{
 
+    public function insert($sql)
+    {   
+        //echo"<br> hello ";
+        $result = mysqli_query($this->conn,$sql);
+        // echo"<br> hello ";
+        if (!$result) {
+            echo mysqli_error($this->conn);
+        }else { 
+            echo"error";        
+            header("Location:..\VIEW\login.php");
+        }
+    }
 
 
 }
