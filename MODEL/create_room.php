aa901f9b2cd8082d06556096b83840fe23e3cb32
Mohammed Elghanam <144786058+MohammedElghanam@users.noreply.github.com>
2023-12-26 12:25:42 +0100

    81

--- MODEL/create_room.php
+++ MODEL/create_room.php
@@ -11,7 +11,7 @@ class create_room extends config{
             echo mysqli_error($this->conn);
         }else { 
             echo"error";        
-            header("Location:..\VIEW\login.php");
+            header("Location:..\VIEW\chat.php");
         }
     }
 
