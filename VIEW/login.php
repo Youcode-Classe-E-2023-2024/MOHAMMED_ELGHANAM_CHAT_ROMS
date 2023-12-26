fa39751ff42233840246a19ad908f533de1ea21e
Mohammed Elghanam <144786058+MohammedElghanam@users.noreply.github.com>
2023-12-26 15:04:34 +0100

    83

--- VIEW/login.php
+++ VIEW/login.php
@@ -17,7 +17,7 @@
   <div class="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
     <h1 class="font-bold text-center text-2xl mb-5">Your Logo</h1>  
     <div class="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
-        <form action="../CONTROLLER/login.php"  method="POST" class="px-5 py-7">
+        <form action="CONTROLLER\login.php"  method="POST" class="px-5 py-7">
             <label class="font-semibold text-sm text-gray-600 pb-1 block">E-mail</label>
             <input type="text" name="email" class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full" required placeholder="entre email"/>
             <label class="font-semibold text-sm text-gray-600 pb-1 block">Password</label>
